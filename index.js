44e35fc785334655f9d02fc43adc8e09b1f716d3
Kingsley Ramos <Kingsleyramos@gmail.com>
2019-11-23 09:54:25 -0800

    Created all functions. Working program. Deleted unused functions and code.

--- index.js
+++ index.js
@@ -1,95 +1,158 @@
 // node modules
 const fs = require("fs");
-const util = require("util");
 const inquirer = require("inquirer");
 const axios = require("axios");
+const pdf = require('html-pdf');
+let pdfOptions = {   
+    height: '2000',
+    width: '1455',
+};
 
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
 
 // Prompt user for GitHub Username and favorite color
 function promptUser() {
-    inquirer.prompt(
+    return inquirer.prompt([
         {
             type: "input",
             name: "username",
             message: "Enter GitHub Username"
+        },
+        {
+            type: "input",
+            name: "color",
+            message: "Enter your favorite color"
         }
-        // {
-        //     type: "input",
-        //     name: "color",
-        //     name: "Enter your favorite color"
-        // }
-    )
-    .then(function({ username }){
-        const gitProfileUrl = `https://api.github.com/users/${username}`;
-        const gitRepoUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
-    
-        axios
-        .get(gitRepoUrl)
-        .then(response => {
-            console.log("# of public Repos: " + response.data.length);
-            //console.log(response.data.);
-
-            let stars = 0;
-            response.data.forEach(function(object)  {
-                stars += object.stargazers_count;
-                console.log("stars: " + stars)
-
-            })
-            console.log("GitHub stars: " + stars);
-
-        });
-
-        axios
-        .get(gitProfileUrl)
-        .then(response =>{
-            let location = response.data.location.replace(/ /g, "+").replace(/,/g, "")
-            console.log("Profile Image: " + response.data.avatar_url);
-            console.log("User name: " + response.data.login);
-            console.log(`Links:
-                User location via Google Map: "https://www.google.com/maps/place/${location}
-                User GitHub Profile: ${response.data.html_url}
-                User Blog: ${response.data.blog}`);
-            console.log("bio: " + response.data.bio);
-            console.log("followers: " + response.data.followers);
-            console.log("following: " + response.data.following);
-        })
-
-    })
+    ]);
 }
 
-promptUser();
-
-// async function start() {
-//     console.log("start")
-//     try {
-//       const answers = await promptUser();
-  
-//       const html = generateHTML(answers);
-  
-//       await writeFileAsync("index.html", html);
-  
-//       console.log("Successfully wrote to index.html");
-//     } catch(err) {
-//       console.log(err);
-//     }
-//   }
-
-// dragen1860
-
-// Profile image = owner.avataar_url
-// user name = owner.login
-// Links to the following:
+async function generateUserInfo(user){
+    const gitProfileUrl = `https://api.github.com/users/${user}`;
 
 
-// User location via Google Maps = 
-// User GitHub profile = 
-// User blog
+    let responseObject = await axios.get(gitProfileUrl);
+
+    return responseObject.data;
+};
 
 
-// User bio = bio
-// Number of public repositories = public_repos
-// Number of followers = followers
-// Number of GitHub stars = stargazers_count
-// Number of users following = following
+async function generateUserRepoStars(user){
+
+    const gitRepoUrl = `https://api.github.com/users/${user}/repos?per_page=1000`;
+
+    const responseObject = await axios.get(gitRepoUrl);
+
+    let stars = 0;
+
+    responseObject.data.forEach(function(element)  {
+        stars += element.stargazers_count;
+    });
+
+    return stars
+}
+
+function generateHTML(userInfo, stars, color) {
+
+    let location = userInfo.location.replace(/ /g, "+").replace(/,/g, "")
+
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Calistoga&display=swap" rel="stylesheet">
+        <style> h1, h2, h3 {text-align: center;} *{font-family: 'Calistoga', cursive;}</style>
+        <title>Document</title>
+    </head>
+    <body style="background: ${color}; height: 1950px; width: 1455px">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-1"></div>
+                <div class="col-sm-10 mt-5">
+                    <div class="card mx-4">
+                        <div class="card-header">
+                                <img class="rounded mx-auto d-block m-4 border-dark" style="width: 300px; border-radius: 50%!important; box-shadow: 0px 0px 40px grey" src="${userInfo.avatar_url}" alt="Profile Image">
+                        </div>
+                        <h1 class="my-5">${userInfo.login}</h1>
+                        <h3 class="mb-5">${userInfo.bio}</h3>
+                        <ul class="list-inline text-center mb-5">
+                            <li class="list-inline-item m-3"><a href="https://www.google.com/maps/place/${location}">${userInfo.location}</a></li>
+                            <li class="list-inline-item m-3"><a href="${userInfo.html_url}">GitHub</a></li>
+                            <li class="list-inline-item m-3"><a href="${userInfo.blog}">Blog</a></li> 
+                        </ul>
+                    </div>
+                    <div class="row text-center">
+                        <div class="card mx-auto my-3" style="width: 400px">
+                            <div class="card-header text-center">
+                                <H3 class="m-0">Public Repositories</H5>
+                            </div>
+                            <div class="card-body text-dark">
+                                <h4 class="card-title text-center">${userInfo.public_repos}</h4>
+                            </div>
+                        </div>
+                        <div class="card mx-auto my-3" style="width: 400px">
+                            <div class="card-header text-center">
+                                <H3 class="m-0">Followers</H5>
+                            </div>
+                            <div class="card-body text-dark">
+                                <h4 class="card-title text-center">${userInfo.followers}</h4>
+                            </div>
+                        </div>
+                        <div class="card mx-auto my-3" style="width: 400px">
+                            <div class="card-header text-center">
+                                <H3 class="m-0">GitHub Stars</H5>
+                            </div>
+                            <div class="card-body text-dark">
+                                <h4 class="card-title text-center">${stars}</h4>
+                            </div>
+                        </div>
+                        <div class="card mx-auto my-3" style="width: 400px">
+                            <div class="card-header text-center">
+                                <H3 class="m-0">Followers</H5>
+                            </div>
+                            <div class="card-body text-dark">
+                                <h4 class="card-title text-center">${userInfo.following}</h4>
+                            </div>
+                        </div>
+                    </div>
+                <div class="col-sm-1"></div>
+            </div>
+            <div class="row">
+            </div>
+        </div>
+    </body>
+    </html>
+    `;
+  }
+
+
+async function start(){
+    console.log("Start...")
+    try{
+        const answers = await promptUser();
+
+        const userInfo = await generateUserInfo(answers.username); //returns object
+        // console.log("login: " + userInfo.login);
+
+        const repoStars = await generateUserRepoStars(answers.username)
+
+        const html = await generateHTML(userInfo, repoStars, answers.color)
+
+
+        pdf.create(html, pdfOptions).toFile(`./${userInfo.login}.pdf`, function(err, res) {
+            if (err) return console.log(err);
+            console.log(res); // { filename: '/app/businesscard.pdf' }
+          });
+
+    }
+    catch (err){
+        console.log(err);
+    }
+}
+
+// ********** RUNNING CODE **********
+
+start();
+ 
